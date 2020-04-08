87da8ef8993828ac5ed92c2b6341d7edada15847
Zack Gomez <zack.gomez@gmail.com>
2020-04-08 13:42:53 -0700

    Input intrigue and mandatory quests

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 type WDRewards = {
   special?: string;
+  cleric?: number;
   fighter?: number;
-  priest?: number;
-  wizard?: number;
   rogue?: number;
+  wizard?: number;
   wild?: number;
   gold?: number;
   intrigue?: number;
@@ -23,9 +23,16 @@ type WDBuilding = {
   owner_rewards: WDRewards;
 };
 
-type WDQuestType = 'Commerce' | 'Warfare' | 'Piety' | 'Arcana' | 'Skullduggery';
+type WDQuestType =
+  | 'Commerce'
+  | 'Warfare'
+  | 'Piety'
+  | 'Arcana'
+  | 'Skullduggery'
+  | 'Mandatory';
+
 type WDQuestCost = {
-  priest?: number;
+  cleric?: number;
   fighter?: number;
   rogue?: number;
   wizard?: number;
@@ -37,14 +44,183 @@ type WDQuest = {
   type: WDQuestType;
   cost: WDQuestCost;
   rewards: WDRewards;
+  plot_reward?: string;
 };
+
+type WDIntrigueCard = {
+  name: string;
+  count: number;
+  effect: string;
+};
+
+type WDMandatoryQuest = {
+  name: string;
+  cost: WDQuestCost;
+  rewards: WDRewards;
+};
+
+const kIntrigueCards: Array<WDIntrigueCard> = [
+  {
+    name: 'Ambush',
+    count: 2,
+    effect: 'opponent_remove_fighter_or_gain_fighter',
+  },
+  {
+    name: 'Arcane Mishap',
+    count: 2,
+    effect: 'opponent_remove_wizard_or_gain_intrigue',
+  },
+  {
+    name: 'Assassination',
+    count: 2,
+    effect: 'opponent_remove_rogue_or_gain_2_gold',
+  },
+  {
+    name: 'Bribe Agent',
+    count: 2,
+    effect: 'play_opponents_location',
+  },
+  {
+    name: 'Free Drinks!',
+    count: 2,
+    effect: 'take_wild_from_opponent',
+  },
+  {
+    name: 'Lack of Faith',
+    count: 2,
+    effect: 'opponent_remove_cleric_or_gain_2_vp',
+  },
+  {
+    name: 'Accelerate Plans',
+    count: 1,
+    effect: 'accelerate_plans',
+  },
+  {
+    name: 'Bidding War',
+    count: 3,
+    effect: 'draw_and_pass_quests',
+  },
+  {
+    name: 'Call for Adventurers',
+    count: 2,
+    effect: 'gain_two_wild_opponents_gain_one',
+  },
+  {
+    name: 'Change of Plans',
+    count: 1,
+    effect: 'discard_quest_for_6vp_opponent_for_3vp',
+  },
+  {
+    name: 'Conscription',
+    count: 2,
+    effect: '2_fighter_give_1_fighter',
+  },
+  {
+    name: 'Crime Wave',
+    count: 2,
+    effect: '2_rogue_give_1_rogue',
+  },
+  {
+    name: 'Good Faith',
+    count: 2,
+    effect: '2_cleric_give_1_cleric',
+  },
+  {
+    name: 'Graduation Day',
+    count: 2,
+    effect: '2_wizard_give_1_wizard',
+  },
+  {
+    name: 'Real Estate Deal',
+    count: 1,
+    effect: 'discard_building_build_free',
+  },
+  {
+    name: 'Recall Agent',
+    count: 2,
+    effect: 'recall_agent',
+  },
+  {
+    name: 'Recruit Spies',
+    count: 1,
+    effect: 'gain_from_supply_and_opponents(2, rogue, 1, 3vp)',
+  },
+  {
+    name: 'Request Assistance',
+    count: 1,
+    effect: 'gain_from_supply_and_opponents(2, fighter, 1, 3vp)',
+  },
+  {
+    name: 'Research Agreement',
+    count: 1,
+    effect: 'gain_from_supply_and_opponents(2, wizard, 1, 5vp)',
+  },
+  {
+    name: 'Summon the Faithful',
+    count: 1,
+    effect: 'gain_from_supply_and_opponents(2, cleric, 1, 5vp)',
+  },
+  {
+    name: 'Sample Wares',
+    count: 2,
+    effect: 'assign_to_builders_hall',
+  },
+  {
+    name: 'Special Assignment',
+    count: 2,
+    effect: 'draw_quests_until_selected_type',
+  },
+  {
+    name: 'Spread the Wealth',
+    count: 2,
+    effect: '4_gold_give_2_gold',
+  },
+  {
+    name: 'Tax Collection',
+    count: 2,
+    effect: 'gain_from_supply_and_opponents(4, gold, 4, 4vp)',
+  },
+];
+
+const kMandatoryQuests = [
+  {
+    name: 'Fend Off Bandits',
+    cost: { fighter: 2, wizard: 1 },
+    reward: { vp: 2 },
+  },
+  {
+    name: 'Foil the Zhentarim',
+    cost: { fighter: 1, rogue: 1, wizard: 1 },
+    rewards: { vp: 2 },
+  },
+  {
+    name: 'Placate Angry Merchants',
+    cost: { cleric: 1, fighter: 1, wizard: 1 },
+    rewards: { vp: 4 },
+  },
+  {
+    name: 'Quell Riots',
+    cost: { cleric: 2, fighter: 1 },
+    rewards: { vp: 4 },
+  },
+  {
+    name: 'Repel Drow Invaders',
+    cost: { cleric: 1, rogue: 1 },
+    rewards: { vp: 2 },
+  },
+  {
+    name: 'Stamp Out Cultists',
+    cost: { cleric: 1, fighter: 1, rogue: 1 },
+    rewards: { vp: 2 },
+  },
+];
 
 const kQuests: Array<WDQuest> = [
   {
     name: 'Lure Artisans of Mirabar',
     type: 'Commerce',
-    cost: { priest: 1, fighter: 1, rogue: 1, gold: 2 },
-    rewards: { special: 'choose free building', vp: 4 },
+    cost: { cleric: 1, fighter: 1, rogue: 1, gold: 2 },
+    rewards: { special: 'build_free', vp: 4 },
   },
   {
     name: 'Train Bladesingers',
@@ -61,19 +237,19 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Convert a Noble to Lathander',
     type: 'Piety',
-    cost: { priest: 2, wizard: 1 },
+    cost: { cleric: 2, wizard: 1 },
     rewards: { special: 'choose_faceup_quest', vp: 8 },
   },
   {
     name: 'Discover Hidden Temple of Lolth',
     type: 'Piety',
-    cost: { priest: 2, fighter: 1, rogue: 1 },
+    cost: { cleric: 2, fighter: 1, rogue: 1 },
     rewards: { special: 'choose_faceup_quest', vp: 10 },
   },
   {
     name: 'Form an Alliance with the Rashemi',
     type: 'Piety',
-    cost: { priest: 2, wizard: 1 },
+    cost: { cleric: 2, wizard: 1 },
     rewards: { special: 'choose_faceup_quest', vp: 10 },
   },
   {
@@ -91,80 +267,90 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Expose Cult Corruption',
     type: 'Skullduggery',
-    cost: { priest: 1, rogue: 4 },
-    rewards: { priest: 2, vp: 4 },
+    cost: { cleric: 1, rogue: 4 },
+    rewards: { cleric: 2, vp: 4 },
   },
   {
     name: 'Produce a Miracle for the Masses',
     type: 'Piety',
-    cost: { priest: 2, gold: 4 },
-    rewards: { special: 'when you get C, trade adv.->C', vp: 5 },
+    cost: { cleric: 2, gold: 4 },
+    rewards: { vp: 5 },
+    plot_reward: 'cleric => trade wild for cleric',
   },
   {
     name: "Explore Ahghairon's Tower",
     type: 'Arcana',
     cost: { fighter: 1, wizard: 2, gold: 2 },
     rewards: { special: 'when you get W, +1 I', vp: 6 },
+    plot_reward: 'intrigue_on_wizard_gain',
   },
   {
     name: 'Fence Goods for Duke of Darkness',
     type: 'Skullduggery',
     cost: { fighter: 1, rogue: 3, gold: 4 },
     rewards: { special: 'when you get R, +2 G', vp: 6 },
+    plot_reward: '2_gold_on_rogue_gain',
   },
   {
     name: 'Bolster Griffon Cavalry',
     type: 'Warfare',
     cost: { fighter: 4, gold: 4 },
     rewards: { special: 'when you get F, +1 F', vp: 6 },
+    plot_reward: 'fighter_on_fighter_gain',
   },
   {
     name: 'Loot the Crypt of Chauntea',
     type: 'Commerce',
-    cost: { priest: 1, rogue: 3, gold: 2 },
+    cost: { cleric: 1, rogue: 3, gold: 2 },
     rewards: { intrigue: 1, special: 'random quest', vp: 7 },
   },
   {
     name: 'Domesticate Owlbears',
     type: 'Arcana',
-    cost: { priest: 1, wizard: 2 },
+    cost: { cleric: 1, wizard: 2 },
     rewards: { fighter: 1, gold: 2, vp: 8 },
   },
   {
     name: 'Study the Illusk Arch',
     type: 'Arcana',
-    cost: { priest: 1, wizard: 2 },
-    rewards: { special: '+2 per future AQ', vp: 8 },
+    cost: { cleric: 1, wizard: 2 },
+    rewards: { vp: 8 },
+    plot_reward: '2vp_per_arcana_quest',
   },
   {
     name: 'Establish New Merchant Guild',
     type: 'Commerce',
-    cost: { priest: 1, fighter: 2, gold: 4 },
-    rewards: { special: '+2 per future CQ', vp: 8 },
+    cost: { cleric: 1, fighter: 2, gold: 4 },
+    rewards: { vp: 8 },
+    plot_reward: '2vp_per_commerce_quest',
   },
   {
     name: 'Protect the House of Wonder',
     type: 'Piety',
-    cost: { priest: 2, fighter: 1, gold: 2 },
-    rewards: { special: '+2 per future PQ', vp: 8 },
+    cost: { cleric: 2, fighter: 1, gold: 2 },
+    rewards: { vp: 8 },
+    plot_reward: '2vp_on_piety_quest',
   },
   {
     name: 'Install a Spy in Castle Waterdeep',
     type: 'Skullduggery',
     cost: { rogue: 4, gold: 4 },
-    rewards: { special: '+2 per future SQ', vp: 8 },
+    rewards: { vp: 8 },
+    plot_reward: '2vp_on_skullduggery_quest',
   },
   {
     name: 'Quell Mercenary Uprising',
     type: 'Warfare',
-    cost: { priest: 1, fighter: 4 },
+    cost: { cleric: 1, fighter: 4 },
     rewards: { special: '+2 per future WQ', vp: 8 },
+    plot_reward: '2vp_on_warfare_quest',
   },
   {
     name: "Infiltrate Builder's Hall",
     type: 'Commerce',
     cost: { fighter: 2, rogue: 2, gold: 4 },
     rewards: { special: '+4 per future building', vp: 6 },
+    plot_reward: '4vp_on_building',
   },
   {
     name: 'Establish Harpers Safe House',
@@ -181,7 +367,7 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Ambush Artor Morlin',
     type: 'Warfare',
-    cost: { priest: 1, fighter: 3, rogue: 1 },
+    cost: { cleric: 1, fighter: 3, rogue: 1 },
     rewards: { gold: 4, vp: 8 },
   },
   {
@@ -193,7 +379,7 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Thin the City Watch',
     type: 'Commerce',
-    cost: { priest: 1, fighter: 1, rogue: 1, gold: 4 },
+    cost: { cleric: 1, fighter: 1, rogue: 1, gold: 4 },
     rewards: { rogue: 4, vp: 9 },
   },
   {
@@ -205,7 +391,7 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Send Aid to the Harpers',
     type: 'Commerce',
-    cost: { priest: 1, fighter: 1, rogue: 1, gold: 4 },
+    cost: { cleric: 1, fighter: 1, rogue: 1, gold: 4 },
     rewards: { special: 'one opponent gains 4g', vp: 15 },
   },
   {
@@ -213,12 +399,14 @@ const kQuests: Array<WDQuest> = [
     type: 'Skullduggery',
     cost: { fighter: 1, rogue: 4, wizard: 1 },
     rewards: { special: '+2 per future played Intrigue', vp: 0 },
+    plot_reward: '2vp_on_intrigue_play',
   },
   {
     name: "Recover the Magister's Orb",
     type: 'Arcana',
     cost: { rogue: 3, wizard: 2 },
-    rewards: { special: 'use 1 occupied bldg per round', vp: 6 },
+    rewards: { vp: 6 },
+    plot_reward: 'use_opponents_space',
   },
   {
     name: 'Steal Spellbook from Silverhand',
@@ -241,13 +429,13 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Eliminate Vampire Coven',
     type: 'Piety',
-    cost: { priest: 2, fighter: 2, rogue: 1 },
+    cost: { cleric: 2, fighter: 2, rogue: 1 },
     rewards: { gold: 4, vp: 11 },
   },
   {
     name: 'Defeat Uprising from Undermountain',
     type: 'Warfare',
-    cost: { priest: 1, fighter: 3, rogue: 1, gold: 2 },
+    cost: { cleric: 1, fighter: 3, rogue: 1, gold: 2 },
     rewards: { fighter: 2, vp: 11 },
   },
   {
@@ -259,14 +447,15 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Investigate Aberrant Infestation',
     type: 'Arcana',
-    cost: { priest: 1, fighter: 1, wizard: 2 },
+    cost: { cleric: 1, fighter: 1, wizard: 2 },
     rewards: { intrigue: 1, vp: 13 },
   },
   {
     name: 'Defend the Tower of Luck',
     type: 'Piety',
-    cost: { priest: 2, fighter: 1, rogue: 1, wizard: 1 },
-    rewards: { special: '1 adventurer per round', vp: 0 },
+    cost: { cleric: 2, fighter: 1, rogue: 1, wizard: 1 },
+    rewards: { vp: 0 },
+    plot_reward: 'wild_per_round',
   },
   {
     name: 'Recruit for Blackstaff Academy',
@@ -277,32 +466,33 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Heal Fallen Gray Hand Soldiers',
     type: 'Piety',
-    cost: { priest: 2, wizard: 1, gold: 4 },
+    cost: { cleric: 2, wizard: 1, gold: 4 },
     rewards: { fighter: 6, vp: 6 },
   },
   {
     name: 'Host Festival for Sune',
     type: 'Arcana',
     cost: { fighter: 2, wizard: 2, gold: 4 },
-    rewards: { priest: 2, vp: 9 },
+    rewards: { cleric: 2, vp: 9 },
   },
   {
     name: "Deliver Weapons to Selune's Temple",
     type: 'Warfare',
     cost: { fighter: 4, rogue: 1, wizard: 1, gold: 2 },
-    rewards: { priest: 2, vp: 9 },
+    rewards: { cleric: 2, vp: 9 },
   },
   {
     name: 'Placate the Walking Statue',
     type: 'Commerce',
-    cost: { priest: 2, rogue: 2, gold: 4 },
+    cost: { cleric: 2, rogue: 2, gold: 4 },
     rewards: { special: 'random free building', vp: 10 },
   },
   {
     name: 'Bribe the Shipwrights',
     type: 'Commerce',
     cost: { rogue: 4, wizard: 1, gold: 4 },
-    rewards: { special: 'when you get G, +1 R', vp: 10 },
+    rewards: { vp: 10 },
+    plot_reward: 'rogue_on_gold_gain',
   },
   {
     name: 'Take Over Rival Organization',
@@ -313,13 +503,13 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Perform the Penance of Duty',
     type: 'Piety',
-    cost: { priest: 2, fighter: 2, gold: 4 },
-    rewards: { priest: 1, fighter: 1, vp: 12 },
+    cost: { cleric: 2, fighter: 2, gold: 4 },
+    rewards: { cleric: 1, fighter: 1, vp: 12 },
   },
   {
     name: 'Repel Seawraiths',
     type: 'Warfare',
-    cost: { priest: 1, fighter: 4, wizard: 1 },
+    cost: { cleric: 1, fighter: 4, wizard: 1 },
     rewards: { gold: 2, vp: 15 },
   },
   {
@@ -331,43 +521,44 @@ const kQuests: Array<WDQuest> = [
   {
     name: "Seal Gate to Cyric's Realm",
     type: 'Piety',
-    cost: { priest: 2, rogue: 3, gold: 4 },
+    cost: { cleric: 2, rogue: 3, gold: 4 },
     rewards: { vp: 20 },
   },
   {
     name: 'Recruit Lieutenant',
     type: 'Warfare',
-    cost: { priest: 1, fighter: 5, rogue: 1, wizard: 1 },
+    cost: { cleric: 1, fighter: 5, rogue: 1, wizard: 1 },
     rewards: { special: 'lieutenant', vp: 0 },
+    plot_reward: 'lieutenant',
   },
   {
     name: 'Recruit Paladins for Tyr',
     type: 'Piety',
-    cost: { priest: 2, fighter: 4, gold: 4 },
-    rewards: { priest: 3, vp: 10 },
+    cost: { cleric: 2, fighter: 4, gold: 4 },
+    rewards: { cleric: 3, vp: 10 },
   },
   {
     name: 'Impersonate Adarbrent Noble',
     type: 'Commerce',
-    cost: { priest: 1, fighter: 2, rogue: 2, wizard: 1, gold: 4 },
+    cost: { cleric: 1, fighter: 2, rogue: 2, wizard: 1, gold: 4 },
     rewards: { intrigue: 2, vp: 18 },
   },
   {
     name: "Expose Red Wizards' Spies",
     type: 'Arcana',
-    cost: { priest: 1, fighter: 1, rogue: 2, wizard: 2, gold: 2 },
+    cost: { cleric: 1, fighter: 1, rogue: 2, wizard: 2, gold: 2 },
     rewards: { intrigue: 1, vp: 20 },
   },
   {
     name: 'Raid on Undermountain',
     type: 'Skullduggery',
-    cost: { priest: 1, fighter: 2, rogue: 4, wizard: 1 },
+    cost: { cleric: 1, fighter: 2, rogue: 4, wizard: 1 },
     rewards: { gold: 2, vp: 20 },
   },
   {
     name: 'Confront the Xanathar',
     type: 'Warfare',
-    cost: { priest: 1, fighter: 4, rogue: 2, wizard: 1 },
+    cost: { cleric: 1, fighter: 4, rogue: 2, wizard: 1 },
     rewards: { gold: 2, vp: 20 },
   },
   {
@@ -379,13 +570,13 @@ const kQuests: Array<WDQuest> = [
   {
     name: 'Ally with House Thann',
     type: 'Commerce',
-    cost: { priest: 1, rogue: 3, wizard: 1, gold: 8 },
+    cost: { cleric: 1, rogue: 3, wizard: 1, gold: 8 },
     rewards: { vp: 25 },
   },
   {
     name: 'Create a Shrine to Oghma',
     type: 'Piety',
-    cost: { priest: 5, gold: 2 },
+    cost: { cleric: 5, gold: 2 },
     rewards: { vp: 25 },
   },
   {
@@ -471,7 +662,7 @@ const kBuildings: Array<WDBuilding> = [
   {
     name: 'House of Heroes',
     build_cost: 8,
-    rewards: { fighter: 2, priest: 1 },
+    rewards: { fighter: 2, cleric: 1 },
     owner_rewards: {
       special: 'choose(1, f, p)',
     },
@@ -480,7 +671,7 @@ const kBuildings: Array<WDBuilding> = [
     name: 'House of the Moon',
     build_cost: 3,
     rewards: {
-      priest: 1,
+      cleric: 1,
       special: 'choose_faceup_quest',
     },
     owner_rewards: {
@@ -567,7 +758,7 @@ const kBuildings: Array<WDBuilding> = [
     name: 'Spires of the Morning',
     build_cost: 4,
     rewards: {
-      special: 'priest_acc',
+      special: 'cleric_acc',
     },
     owner_rewards: {
       vp: 2,
@@ -597,7 +788,7 @@ const kBuildings: Array<WDBuilding> = [
     name: 'The Tower of Luck',
     build_cost: 8,
     rewards: {
-      priest: 1,
+      cleric: 1,
       rogue: 2,
     },
     owner_rewards: {
@@ -638,7 +829,7 @@ const kBuildings: Array<WDBuilding> = [
     name: 'The Zoarstar',
     build_cost: 8,
     rewards: {
-      special: 'play_in_opponents_space',
+      special: 'use_opponents_space',
     },
     owner_rewards: {
       vp: 2,
@@ -656,7 +847,7 @@ const kBaseLocations: Array<WDLocation> = [
   {
     name: 'The Plinth',
     rewards: {
-      priest: 1,
+      cleric: 1,
     },
   },
   {
